d90701931ed1849164c9bebddbbfbd7c68dc271f
Prasad Shelke <shelkeprasad1102@gmail.com>
2026-02-08 19:22:31 +0530

    added sidekiq setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  require "sidekiq/web"
+  mount Sidekiq::Web => "/sidekiq"
+
   get "up" => "rails/health#show", as: :rails_health_check
+
   resources :imports, only: [:create, :index, :show]
 end

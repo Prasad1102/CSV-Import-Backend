c56667d28eb10adadf980228344dc260995d0927
Prasad Shelke <shelkeprasad1102@gmail.com>
2026-02-08 18:23:29 +0530

    added a show method in imports controller

--- app/controllers/imports_controller.rb
+++ app/controllers/imports_controller.rb
@@ -1,13 +1,26 @@
 class ImportsController < ApplicationController
     def create
-        @import = Import.new(file: params[:file])
-
+        @import =Import.new(file: params[:file])
         if @import.save
             render json: {
             id: @import.id,
             }, status: :created
         else
             render json: @import.errors, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        @import = Import.find(params[:id])
+        if @import
+            render json: {
+                id: @import.id,
+                status: @import.status,
+                failed_count: @import.failed_count,
+                total_count: @import.total_count,
+                process_count: @import.process_count,
+                file_url: rails_blob_url(@import.file)  
+            }
         end
     end
 end
